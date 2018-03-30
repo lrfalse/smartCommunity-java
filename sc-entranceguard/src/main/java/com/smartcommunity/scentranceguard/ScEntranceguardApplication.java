c0296e809db58b9a4145df72df91ca3a28f7aee1
lrfalse <675927961@qq.com>
2018-03-30 12:01:37 +0800

    初始导入

---
+++ sc-entranceguard/src/main/java/com/smartcommunity/scentranceguard/ScEntranceguardApplication.java
@@ -0,0 +1,12 @@
+package com.smartcommunity.scentranceguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScEntranceguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScEntranceguardApplication.class, args);
+	}
+}
