ad963596faa5324982bd504e9fd0e359bd6f6b96
lrfalse <675927961@qq.com>
2018-04-04 18:31:53 +0800

    新增配置

--- sc-linlihouse/src/main/java/com/linlihouse/controller/TestController.java
+++ sc-linlihouse/src/main/java/com/linlihouse/controller/TestController.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author(作者) :lrfalse<wangliyou>
  * @date (开发日期) :2018/4/2 22:06
  **/
-@RestController
+@Controller
 @RequestMapping("/")
 public class TestController extends BaseApi{
 
@@ -29,10 +29,15 @@ public class TestController extends BaseApi{
 	  * @author(作者): lrfalse<wangliyou>
 	  * @date (开发日期): 2018/4/3 18:17
 	  **/
+	@ResponseBody
 	@RequestMapping(value = "/test")
 	public HttpResults httpResultstest(HttpServletRequest request) throws Exception {
 		IsJsonDTO isJson=(IsJsonDTO)request.getAttribute("preHandleJsonDto");
 		HttpResults test=getHttpResult(new TestDto());
 		return test;
+	}
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "test";
 	}
 }
