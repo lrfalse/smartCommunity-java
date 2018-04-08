e16b9642dd1972e6e569a1880e1a9ae7ef2768a5
lrfalse <675927961@qq.com>
2018-04-08 11:07:15 +0800

    新增配置

--- sc-tradingArea/src/main/java/com/linliHouse/TradingAreaApplication.java
+++ sc-tradingArea/src/main/java/com/linliHouse/TradingAreaApplication.java
@@ -1,4 +1,4 @@
-package com.tradingArea;
+package com.linliHouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- sc-tradingArea/src/test/java/com/linliHouse/ScLinlihouseApplicationTests.java
+++ sc-tradingArea/src/test/java/com/linliHouse/ScLinlihouseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tradingArea;
+package com.linliHouse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
