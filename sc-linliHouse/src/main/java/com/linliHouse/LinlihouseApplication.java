fc704a191a125512890a0ae2478b017ecddafbd4
lrfalse <675927961@qq.com>
2018-04-08 11:05:58 +0800

    数据还原

---
+++ sc-linliHouse/src/main/java/com/linliHouse/LinlihouseApplication.java
@@ -0,0 +1,12 @@
+package com.linliHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinlihouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinlihouseApplication.class, args);
+	}
+}
