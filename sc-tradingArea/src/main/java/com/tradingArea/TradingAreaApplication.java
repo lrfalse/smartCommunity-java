a8ed7a62f9d55509fe2cff802e52094045d39c80
lrfalse <675927961@qq.com>
2018-04-08 11:21:27 +0800

    新增超级商圈

---
+++ sc-tradingArea/src/main/java/com/tradingArea/TradingAreaApplication.java
@@ -0,0 +1,12 @@
+package com.tradingArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAreaApplication.class, args);
+	}
+}
