3bef762bf249e0a17ac7f9d281c3191091132916
lrfalse <675927961@qq.com>
2018-04-08 10:53:27 +0800

    新增个人中心体系模块

---
+++ sc-personalCenter/src/main/java/com/personalCenter/PersonalCenterApplication.java
@@ -0,0 +1,22 @@
+package com.personalCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @Description(功能描述): 超级商圈
+  * @author(作者): lrfalse<wangliyou>
+  * @date (开发日期): 2018/4/8 10:20
+  **/
+@SpringBootApplication
+public class PersonalCenterApplication {
+
+	/**
+	  * @Description(功能描述): 超级商圈启动项
+	  * @author(作者): lrfalse<wangliyou>
+	  * @date (开发日期): 2018/4/8 10:20
+	  **/
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCenterApplication.class, args);
+	}
+}
