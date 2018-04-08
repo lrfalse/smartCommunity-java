99851ee6a2d2d038cda6fe4e1c0acb83cb0232ac
lrfalse <675927961@qq.com>
2018-04-08 10:23:59 +0800

    新增超级商圈

---
+++ sc-tradingArea/src/test/java/com/tradingArea/ScLinlihouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tradingArea;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScLinlihouseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
