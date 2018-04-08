f5cc193f416c7f39df87ba4d540a3e0b08dc2487
lrfalse <675927961@qq.com>
2018-04-08 11:11:41 +0800

    新增邻里

---
+++ sc-linli/src/main/java/com/linli/controller/TestController.java
@@ -0,0 +1,39 @@
+package com.linli.controller;
+
+import com.commons.controller.BaseApi;
+import com.commons.dto.HttpResults;
+import com.commons.dto.IsJsonDTO;
+import com.commons.dto.TestDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description(功能描述) :测试
+ * @author(作者) :lrfalse<wangliyou>
+ * @date (开发日期) :2018/4/2 22:06
+ **/
+@Controller
+@RequestMapping("/")
+public class TestController extends BaseApi{
+
+
+	/**
+	  * @Description(功能描述): 测试demo
+	  * @author(作者): lrfalse<wangliyou>
+	  * @date (开发日期): 2018/4/3 18:17
+	  **/
+	@ResponseBody
+	@RequestMapping(value = "/test")
+	public HttpResults httpResultstest(HttpServletRequest request) throws Exception {
+		IsJsonDTO isJson=(IsJsonDTO)request.getAttribute("preHandleJsonDto");
+		HttpResults test=getHttpResult(new TestDto());
+		return test;
+	}
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "test";
+	}
+}
