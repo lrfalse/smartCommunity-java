7600d813c4938537cddb4fbbf6d87379524d083f
lrfalse <675927961@qq.com>
2018-04-24 09:40:22 +0800

    框架优化。

--- sc-entranceguard/src/main/java/com/scentranceguard/EntranceguardApplication.java
+++ sc-entranceguard/src/main/java/com/scentranceguard/EntranceguardApplication.java
@@ -1,12 +1,26 @@
 package com.scentranceguard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+/**
+  * @Description(功能描述): 门禁服务系统
+  * @author(作者): lrfalse<wangliyou>
+  * @date (开发日期): 2018/4/24 9:18
+  **/
 @SpringBootApplication
-public class EntranceguardApplication {
+public class EntranceguardApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(EntranceguardApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntranceguardApplication.class, args);
+	}
+	@Override
+	public void run(String... args)  {
+		logger.info("门禁服务系统启动完成!");
 	}
 }
