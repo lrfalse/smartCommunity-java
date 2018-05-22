8b93fffbbe30a14ad03861e6068b569bc3df0257
lrfalse <675927961@qq.com>
2018-05-22 14:31:18 +0800

    新增菜单管理

--- sc-admin/sc-linliAdmin/src/main/java/com/admin/controller/permission/SysMenuController.java
+++ sc-admin/sc-linliAdmin/src/main/java/com/admin/controller/permission/SysMenuController.java
@@ -4,9 +4,8 @@ import com.commons.controller.BaseSysApi;
 import com.commons.dto.HttpResults;
 import com.commons.entity.sys.SysUserEntity;
 import com.commons.service.RedisService;
-import com.commons.service.sys.SysUserService;
+import com.commons.service.sys.SysMenuService;
 import com.commons.utils.CommonUtils;
-import com.commons.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 /**
- * @Description(功能描述) :用户信息
+ * @Description(功能描述) :菜单
  * @author(作者) :lrfalse<wangliyou>
  * @date (开发日期) :2018/5/10 8:49
  **/
@@ -22,46 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class SysMenuController extends BaseSysApi {
 
 	@Autowired
-	private SysUserService sysUserService;
+	private SysMenuService sysMenuService;
 	@Autowired
 	private RedisService redisService;
 
 	/**
-	  * @Description(功能描述): 后台用户登录
+	  * @Description(功能描述): 根据用户获取菜单权限
 	  * @author(作者): lrfalse<wangliyou>
-	  * @date(开发日期): 2018/5/10 8:55
+	  * @date(开发日期): 2018/5/14 10:24
 	  **/
-	@PostMapping("/login")
-	public HttpResults login(@RequestBody SysUserEntity sysUser) throws Exception {
-		if (CommonUtils.isNotEmpty(sysUser.getPwd()) && CommonUtils.isNotEmpty(sysUser.getName())) {
-			sysUser = sysUserService.login(sysUser);
-			if (CommonUtils.isNotEmpty(sysUser)) {
-				String userJsonStr = JsonUtils.toJson(sysUser);
-				redisService.set(sysUser.getName(), userJsonStr, 30);
-				SysUserEntity returninfo =new SysUserEntity();
-				returninfo.setName(sysUser.getName());
-				returninfo.setMobPhone(sysUser.getMobPhone());
-				returninfo.setImgUrl(sysUser.getImgUrl());
-				returninfo.setNickName(sysUser.getNickName());
-				return getHttpResult(returninfo);
-			}
+	@PostMapping("/findMenu")
+	public HttpResults findMenu(@RequestBody SysUserEntity sysUser) throws Exception {
+		if (CommonUtils.isNotEmpty(sysUser.getName())) {
 			return getHttpResultError();
 		}
 		return getHttpResultError();
 	}
-
-	/** 
-	  * @Description(功能描述): 新增系统用户
-	  * @author(作者): lrfalse<wangliyou>
-	  * @date(开发日期): 2018/5/10 10:43
-	  **/ 
-	@PostMapping("/addSysUser")
-	public HttpResults addSysUser(@RequestBody SysUserEntity sysUser){
-		if(CommonUtils.isNotEmpty(sysUser.getPwd(),sysUser.getMobPhone(),sysUser.getNickName())){
-			int result=sysUserService.saveSysUser(sysUser);
-			return getHttpResult(result);
-		}
-		return getHttpResultError();
-	}
-
 }
